c31257347743fddf06eddbdfa8849ea619cdf6a2
Quentin DESTEFANIS <93386446+Norfu@users.noreply.github.com>
2023-12-18 17:08:40 +0100

    up

--- src/App.js
+++ src/App.js
@@ -17,27 +17,11 @@ const TodoList = () => {
       });
   }, [isMounted]);
 
-  const submit = e => {
-    e.preventDefault()
-    fetch('https://gsym8ugyub.execute-api.eu-central-1.amazonaws.com/dev/todo_list', {
-      method: 'POST',
-      body: JSON.stringify({ todoForm }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(res => res.json())
-      .then(json => setTodoForm(json.todoForm))
-  }  
   }
   return (
   
     <><h1>Branche Prod</h1><ul>
-      <form onSubmit={submit}>
-        <input type="text"
-        name="text"
-        value={todoForm.text}
-        onChange={e => setTodoForm({...todoForm, text:e.target.value})}/>
-        <input type="submit" name="ajouter"/>
-      </form>
+  
       {Todos.map((Todo, index) => {
         return (
           <li key={index}>
